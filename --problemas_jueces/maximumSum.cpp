9644de53b089e2d76962080e36de56e2bef92394
Mickysaurio-rex <80714939+Mickysaurio-rex@users.noreply.github.com>
2021-08-28 20:34:00 -0400

    Create maximumSum.cpp

---
+++ --problemas_jueces/maximumSum.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace{
+typedef long long LL;
+typedef vector<int> vint;
+typedef pair<int,int> pint;
+typedef unsigned long long ULL;
+
+int CC_;
+#define sf scanf
+#define pf printf
+#define PP cin.get();
+#define NL cout<<endl;
+#define all(container) container.begin(),container.end()
+#define DC(x_) cout<<">>> "<<#x_<<"\n";DA(x_.begin(), x_.end());
+#define DD(x_) cout<<">>>>( "<<++CC_<<" ) "<<#x_<<": "<<x_<<endl;
+#define SS printf(">_<LOOOOOK@MEEEEEEEEEEEEEEE<<( %d )>>\n",++CC_);
+#define EXT(st_) cout<<"\n>>>Exicution Time: "<<(double)(clock()-st_)/CLOCKS_PER_SEC<<endl;
+#define DM(MT,n_,m_)pf("Matrix %s:\n   ", #MT);for(int i_= 0;i_<m_;i_++)pf("%4d ", i_);NL;NL;for(int r_=0;r_<n_;r_++){pf("%2d ", r_);for(int c_= 0;c_<m_;c_++)pf("%4d ", MT[r_][c_]);NL}
+#define mem(a_,b_)(a_, b_, sizeof(a_));
+
+
+const double EPS= 1E-9;
+const double PI= 2*acos(0.0);
+const long long MOD= 1000000007;
+
+template <class T>void DA(T i,T e){while(i!=e){cout<<"Con>>( "<<++CC_<<" ) "<<*i++<<endl;}}
+template <class T>void DA(T* x, int l){for(int i=0; i<l;i++)cout<<"["<<i<<"]>> "<<x[i]<<endl;}
+}
+
+const int sss= 20+ 1E5;
+
+int a[sss];
+
+
+
+pint arbol[4*sss];
+
+void construir(int hi, int l, int r){
+    if(l == r){
+        arbol[hi]= pint(a[l], l);
+        return;
+    }
+    
+    int m= (l+r)/2;
+    
+    construir(2*hi, l, m);
+    construir(2*hi+1, m+1, r);
+    
+    arbol[hi]= max(arbol[2*hi],arbol[2*hi+1]);
+}
+
+pint query(int hi, int l, int r, int st, int nd){
+    if(l >= st && r <= nd){
+        return arbol[hi];
+    }
+    
+    if(l > nd || r < st){
+        return pint(INT_MIN, INT_MIN);
+    }
+    
+    int m= (l+r)/2;
+    
+    pint a, b;
+    a= query(2*hi, l, m, st, nd);
+    b= query(2*hi+1, m+1, r, st, nd);
+    
+    return max(a, b);
+}
+
+void update(int hi, int l, int r, int idx, int x){
+    if(l == r && l == idx){
+        arbol[hi].first= x;
+        a[l]= x;
+        return;
+    }
+    
+    int m= (l+r)/2;
+    
+    if(m >= idx) update(2*hi, l, m, idx, x);
+    else update(2*hi+1, m+1, r, idx, x);
+    
+    arbol[hi]= max(arbol[2*hi],arbol[2*hi+1]);
+}
+
+void solve(void){
+    int n, q;
+    
+    sf("%d", &n);
+    for(int i= 1; i<=n; i++){
+        sf("%d", &a[i]);
+    }
+    
+    construir(1, 1, n);
+    
+    char com, dump;
+    int l, r, x, idx;
+    
+    sf("%d", &q);
+    while(q--){
+//        sf("%c", &com);
+        cin>>com;
+        if(com == 'Q'){
+            sf("%d%d", &l, &r);
+            //index of first max
+            int fmx= query(1,1,n,l,r).second;
+            int smx1= INT_MIN, smx2= INT_MIN;
+            
+            if(fmx != l){
+                smx1 = query(1,1,n, l, fmx-1).first;
+            }
+            if(fmx != r){
+                smx2 = query(1,1,n, fmx+1, r).first;
+            }
+            
+            pf("%d\n", a[fmx] + max(smx1, smx2));
+        }
+        else if(com == 'U'){
+            sf("%d%d", &idx, &x);
+            update(1, 1, n, idx, x);
+        }
+        
+    }
+    
+}
+
+
+
+int main(void){
+    solve();
+    
+    return 0;
+}
